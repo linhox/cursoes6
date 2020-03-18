cdbce0614bcb7c1a377cb073d8981386fb186a7c
Paulo Vítor <paulovmedeiros@outlook.com>
2020-03-18 14:29:56 -0300

    Organização final após fim do curso.

---
+++ Módulo 3/aula1.js
@@ -0,0 +1,13 @@
+//Async/Await
+
+const minhaPromise = () => new Promise((resolve, reject) =>{
+  setTimeout(() => { resolve('OK') }, 2000);
+});
+
+async function executaPromise() {
+  const response = await minhaPromise();
+
+  console.log(response);
+}
+
+executaPromise();
